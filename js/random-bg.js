ce0bd70cd7ab5e66521774a14ae8908970f75c57
qi tang <1934036840@qq.com>
2026-05-29 19:47:57 +0800

    Site updated: 2026-05-29 19:47:56

--- js/random-bg.js
+++
@@ -1,5 +0,0 @@
-(function() {
-  var bgList = ['/images/bg/1.png', '/images/bg/2.png', '/images/bg/3.png', '/images/bg/4.png'];
-  var idx = Math.floor(Math.random() * bgList.length);
-  document.documentElement.style.setProperty('--random-bg', 'url(' + bgList[idx] + ')');
-})();
